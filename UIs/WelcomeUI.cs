efc04329e2feb7bfc018305566b745b0c13b2435
TungusSs <nikitakuznetsovgg@gmail.com>
2024-06-20 08:57:31 +0300

    Updated URLs for cards

--- UIs/WelcomeUI.cs
+++ UIs/WelcomeUI.cs
@@ -13,9 +13,9 @@ namespace PCInfos
 
         private void AttachButtonHandlers()
         {
-            panelSystemClearButton.Click += (sender, e) => OpenUrl("https://example.com/system-clear-tips");
-            panelVirusesClearButton.Click += (sender, e) => OpenUrl("https://example.com/virus-clear-tips");
-            panelTempFilesButton.Click += (sender, e) => OpenUrl("https://example.com/temp-files-clear-tips");
+            panelSystemClearButton.Click += (sender, e) => OpenUrl("https://dtf.ru/howto/256417-ultimativnyi-gaid-po-nastroike-i-optimizacii-windows-10");
+            panelVirusesClearButton.Click += (sender, e) => OpenUrl("https://www.kaspersky.ru/resource-center/threats/how-to-get-rid-of-a-computer-virus");
+            panelTempFilesButton.Click += (sender, e) => OpenUrl("https://support.microsoft.com/ru-ru/windows/%D0%BE%D1%81%D0%B2%D0%BE%D0%B1%D0%BE%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BC%D0%B5%D1%81%D1%82%D0%B0-%D0%BD%D0%B0-%D0%B4%D0%B8%D1%81%D0%BA%D0%B5-%D0%B2-windows-a18fae02-a0fa-8df9-9838-8970f9939de4");
         }
 
         private void OpenUrl(string url)
